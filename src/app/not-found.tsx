5c17aa2489df477e8f7add85bc74a2375f5f25fd
Robel Tekle <robel.tekle@gmail.com>
2025-09-10 18:27:26 +0200

    style(Font): add Quicksand font-family

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+// TODO: create a better 404 page
+export default function NotFound() {
+
+  return (
+    <div className="h-full flex items-center justify-center">
+      <h1>404 - Not Found</h1>
+    </div>
+  );
+}
