d2d62338600593f7171a37af1130346dec1734ed
Robel Tekle <robel.tekle@gmail.com>
2025-09-17 10:27:06 +0200

    chore(ESLint Prettier): update config for lint and workspace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,27 @@
-import type { Metadata } from "next";
-import { Quicksand } from "next/font/google";
-import "./globals.css";
-
-
+import type { Metadata } from 'next'
+import { Quicksand } from 'next/font/google'
+import './globals.css'
 
 const quickSand = Quicksand({
-  subsets: ["latin"],
-  weight: ["400", "500", "600"],
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
   // variable: "--font-quicksand",
-});
+})
 
 // TODO: update metadata
 export const metadata: Metadata = {
   title: "Amandine D'Elia",
-  description: "Médecine Traditionnelle Chinoise & Acupuncture",
-};
+  description: 'Médecine Traditionnelle Chinoise & Acupuncture',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="fr">
-      <body
-        className={`${quickSand.className} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${quickSand.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,9 @@
 
 // TODO: create a better 404 page
 export default function NotFound() {
-
   return (
     <div className="h-full flex items-center justify-center">
       <h1>404 - Not Found</h1>
     </div>
-  );
+  )
 }
