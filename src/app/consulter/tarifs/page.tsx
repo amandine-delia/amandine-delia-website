6618d271c1bd4df19daa8cc2c8b4c9e5d5bae2f2
Robel Tekle <robel.tekle@gmail.com>
2025-10-15 14:53:59 +0200

    feat(Tarifs): init page Tarifs

--- src/app/consulter/tarifs/page.tsx
+++ src/app/consulter/tarifs/page.tsx
@@ -1,9 +1,70 @@
+import { Container } from '@/components/Container'
+import { H1, H2, P, Strong, Ul } from '@/components/Text'
+
 export default function TarifsPage() {
   return (
-    <div>
-      <h1>Tarifs des Consultations</h1>
-      <p>Voici les tarifs appliqués pour les consultations en médecine traditionnelle chinoise.</p>
-      {/* Contenu des tarifs */}
-    </div>
+    <Container className="px-4 pt-8 pb-24">
+      <H1 className="mb-8">Tarifs et modalités de paiement</H1>
+
+      <div className="flex flex-col gap-y-8">
+        <div>
+          <H2 className="mb-3" customColor="text-black text-lg">
+            Consultations individuelles
+          </H2>
+          <Ul>
+            <li>
+              Première séance (1h15 à 1h30) :{' '}
+              <Strong>
+                70 € en tarif plein puis dégressif selon les situations (revenus, patrimoine), voir
+                avec votre praticienne
+              </Strong>
+            </li>
+
+            <li>
+              Séance de suivi (1h) : <Strong>60 €</Strong>
+            </li>
+          </Ul>
+        </div>
+
+        <div>
+          <H2 className="mb-3" customColor="text-black text-lg">
+            Moyens de paiement acceptés
+          </H2>
+          <Ul>
+            <li>Chèques (à l’ordre d’Amandine Delia)</li>
+            <li>Espèces</li>
+            <li>Virement bancaire (RIB sur demande)</li>
+            <li>⚠️ Le cabinet ne prend pas la CB</li>
+          </Ul>
+        </div>
+
+        <div>
+          <H2 className="mb-3" customColor="text-black text-lg">
+            Facture pour remboursement mutuelle
+          </H2>
+          <div className="flex flex-col gap-y-3">
+            <P>
+              Certaines mutuelles prennent en charge une partie des séances d’acupuncture ou de
+              Médecine Traditionnelle Chinoise, dans le cadre des médecines douces.
+            </P>
+            <P>
+              Une <Strong>facture</Strong> nominative peut être délivrée à chaque séance afin de
+              faciliter vos démarches de remboursement auprès de votre complémentaire santé.
+            </P>
+
+            <P>
+              👉 Renseignez-vous directement auprès de votre mutuelle pour connaître les modalités
+              de prise en charge des séances d&apos;acupuncture faites par une praticienne certifiée
+              en médecine chinoise.
+            </P>
+
+            <P>
+              Notre praticienne n&apos;est pas médecin et à ce titre, les soins ne sont pas pris en
+              charge par la sécurité sociale.
+            </P>
+          </div>
+        </div>
+      </div>
+    </Container>
   )
 }
