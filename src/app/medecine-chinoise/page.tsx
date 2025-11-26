c0a40281a6a75a3772044a8a8bca18eddb4d3c8d
Robel Tekle <robel.tekle@gmail.com>
2025-11-26 19:54:25 +0100

    feat(API Med Chinoise Page): add page data to config & connect & style markdown

--- src/app/medecine-chinoise/page.tsx
+++ src/app/medecine-chinoise/page.tsx
@@ -1,5 +1,8 @@
 import { Container } from '@/components/Container'
-import { P, PageTitle, Strong } from '@/components/Text'
+import { ErrorMessage } from '@/components/ErrorMessage'
+import { Markdown } from '@/components/Markdown'
+import { PageTitle } from '@/components/Text'
+import { getPageData, parseJsonData } from '@/utils/fetchData'
 import type { Metadata } from 'next'
 import Image from 'next/image'
 import mtc from '../../../public/img/pages/mtc.jpg'
@@ -13,6 +16,9 @@ export const metadata: Metadata = {
 }
 
 export default function MedecineChinoisePage() {
+  const pageData = getPageData('data/pages/med-chinoise.json')
+  const { data, isError } = parseJsonData<{ body: string }>(pageData)
+
   return (
     <Container className="px-4 pt-8 pb-24">
       <PageTitle>La Médecine Traditionnelle Chinoise</PageTitle>
@@ -34,32 +40,8 @@ export default function MedecineChinoisePage() {
         />
       </div>
 
-      <div>
-        <P className=" mb-8">
-          La Médecine Traditionnelle Chinoise est une pratique millénaire qui considère l’être
-          humain dans sa globalité. Elle ne se limite pas à un symptôme : elle cherche à comprendre
-          l’équilibre entre le corps, l’esprit et les émotions, afin d’accompagner chacun vers plus
-          d’harmonie.
-        </P>
-
-        <P className=" mb-8">
-          Au cœur de cette approche se trouve le <Strong>Qi</Strong>, l’énergie vitale qui circule
-          en nous. Lorsqu’elle circule librement, nous nous sentons en bonne santé. La MTC s’appuie
-          aussi sur les notions de <Strong>Yin et Yang</Strong>, ces forces complémentaires qui
-          doivent rester équilibrées, et sur les <Strong>Cinq Éléments</Strong> (Bois, Feu, Terre,
-          Métal, Eau), qui reflètent les rythmes de la nature et nos propres cycles de vie.
-        </P>
-
-        <P className=" mb-8">
-          Pour favoriser cet équilibre, différentes méthodes peuvent être utilisées :{' '}
-          <Strong>acupuncture</Strong>, <Strong>moxibustion</Strong>, <Strong>massage Tuina</Strong>
-          , <Strong>diététique chinoise</Strong> et <Strong>Qi Gong</Strong>.
-        </P>
-
-        <P>
-          Son but est simple : vous aider à retrouver vitalité, sérénité et bien-être, en douceur et
-          toujours dans le respect de votre singularité.
-        </P>
+      <div className="mt-4 md:mt-8">
+        {data && !isError ? <Markdown>{data?.body}</Markdown> : <ErrorMessage />}
       </div>
     </Container>
   )
