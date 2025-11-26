18a5546721f37b5002fd5a1c0a1650097ed9f7ea
Robel Tekle <robel.tekle@gmail.com>
2025-11-26 18:20:00 +0100

    feat(API Moxibustion): add page data to config & connect & style markdown

--- src/app/soins/moxibustion/page.tsx
+++ src/app/soins/moxibustion/page.tsx
@@ -1,8 +1,11 @@
 import { Breadcrumb } from '@/components/Breadcrumb'
 import { Container } from '@/components/Container'
-import { P, PageTitle, Ul } from '@/components/Text'
+import { PageTitle } from '@/components/Text'
 import type { Metadata } from 'next'
 
+import { ErrorMessage } from '@/components/ErrorMessage'
+import { Markdown } from '@/components/Markdown'
+import { getPageData, parseJsonData } from '@/utils/fetchData'
 import Image from 'next/image'
 import moxibustion from '../../../../public/img/pages/moxibustion.jpg'
 
@@ -14,6 +17,9 @@ export const metadata: Metadata = {
 }
 
 export default function MoxibustionPage() {
+  const pageData = getPageData('data/pages/moxibustion.json')
+  const { data, isError } = parseJsonData<{ body: string }>(pageData)
+
   return (
     <Container className="px-4 pt-8 pb-24">
       <Breadcrumb step3="Moxibustion" />
@@ -37,27 +43,8 @@ export default function MoxibustionPage() {
         />
       </div>
 
-      <div>
-        <P className=" mb-8">
-          La moxibustion est une pratique traditionnelle de la médecine chinoise qui utilise la
-          chaleur douce de l’armoise (plante séchée et roulée en bâton ou en cône). Cette chaleur
-          est appliquée près de points d’acupuncture afin de stimuler la circulation de l’énergie
-          (le Qi) et d’apporter confort et vitalité.
-        </P>
-
-        <P className=" mb-8">Ce soin est particulièrement apprécié pour :</P>
-
-        <Ul className="mb-8">
-          <li>soulager les tensions et les douleurs,</li>
-          <li>réchauffer le corps et soutenir l’énergie vitale,</li>
-          <li>renforcer les défenses naturelles,</li>
-          <li>favoriser détente et bien-être profond.</li>
-        </Ul>
-
-        <P>
-          La moxibustion est une approche douce, naturelle et non invasive, qui complète
-          harmonieusement d’autres pratiques de santé et de prévention.
-        </P>
+      <div className="mt-4 md:mt-8">
+        {data && !isError ? <Markdown>{data?.body}</Markdown> : <ErrorMessage />}
       </div>
     </Container>
   )
